67b9753f89dca8129ac0fdec639e20a41f84e1af
womamp <mams260405@gmail.com>
2025-04-13 08:59:56 +0700

    Delete crud_php directory

--- crud_php/API/create.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../config/database.php";
-$data = json_decode(file_get_contents("php://input"), true);
-$sql = "INSERT INTO items (name, description) VALUES ('{$data['name']}', '{$data['description']}')";
-$conn->query($sql);
-echo json_encode(["message" => "Data ditambahkan"]);
-?>
